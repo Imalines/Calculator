edd86f53e863da7ff4ddae6b628dae7e4025c557
Imalines <59148408+Imalines@users.noreply.github.com>
2019-12-23 12:46:36 +0500

    Update Testing.java

--- src/Testing.java
+++ src/Testing.java
@@ -3,22 +3,26 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+import java.util.Scanner;
+
 import org.junit.jupiter.api.Test;
 
 class Testing {
 	Calculate calc = new Calculate();
 	Robot r;
 	
-	int[] inputTest1 = {KeyEvent.VK_1,KeyEvent.VK_R,KeyEvent.VK_COMMA,KeyEvent.VK_5};
-	int[] inputTest2 = {KeyEvent.VK_COMMA,KeyEvent.VK_0,KeyEvent.VK_COMMA,KeyEvent.VK_5};
+	int[] inputTest1 = {KeyEvent.VK_1,KeyEvent.VK_R,'.',KeyEvent.VK_5};
+	int[] inputTest2 = {'.',KeyEvent.VK_0,'.',KeyEvent.VK_5};
 	int[] inputTest3 = {KeyEvent.VK_1,KeyEvent.VK_0,KeyEvent.VK_2,KeyEvent.VK_BACK_SPACE};
 	
 	@Test
 	void test1() {
+		System.out.println("Test1");
 		try {
 			r = new Robot();
 			r.delay(100);
-		} catch (AWTException e) {
+		}
+		catch (AWTException e) {
 			e.printStackTrace();
 		}
 		for (int key:inputTest1) {
@@ -29,6 +33,7 @@ class Testing {
 	}
 	@Test
 	void test2() {
+		System.out.println("Test2");
 		try {
 			r = new Robot();
 			r.delay(100);
@@ -52,6 +57,7 @@ class Testing {
 	}
 	@Test
 	void test3() {
+		System.out.println("Test3");
 		try {
 			r = new Robot();
 			r.delay(100);
